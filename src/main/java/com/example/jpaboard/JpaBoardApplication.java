b02544f2c00f06bd495a45ed955dc12f6cbf37c4
김영훈 <hunifive@gmail.com>
2025-08-06 13:42:35 +0900

    init

---
+++ src/main/java/com/example/jpaboard/JpaBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpaboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaBoardApplication.class, args);
+    }
+
+}
